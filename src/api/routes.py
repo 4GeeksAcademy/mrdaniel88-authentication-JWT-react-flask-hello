e816c802cf6009900a88d0cf53572b68733ec466
Daniel Martinez <d.martinez74@gmail.com>
2023-05-23 01:12:27 +0000

    login, signup, protected added

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,9 +4,12 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
 api = Blueprint('api', __name__)
-
+app=Flask(__name__)
+bcrypt=Bcrypt(app)
 
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
@@ -16,3 +19,44 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route("/signup", methods=["POST"])
+def user_creato():
+    data = request.get_json()
+    print(data)
+    new_user = User.query.filter_by(email=data["email"]).first()
+    if (new_user is not None):
+        return jsonify(
+            {
+                "msg":"Email registrado"
+            }
+        ),400
+    secure_password=bcrypt.generate_password_hash(data["password"],rounds=None).decode("utf-8")
+    print (new_user is None)
+    new_user = User(email=data["email"], password=secure_password, is_active=True)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify(new_user.serialize()), 201
+
+@api.route("/login", methods=["POST"])
+def user_login():
+    user_email = request.json.get("email")
+    user_password = request.json.get("password")
+    #Buscar usuario por correo
+    user=User.query.filter_by(email=user_email).first()
+    if user is None:
+        return jsonify({"Message":"User not found"}), 401
+    
+    #Verificar la clave
+    if not bcrypt.check_password_hash(user.password,user_password):
+        return jsonify({"message":"Wrong password"}), 401
+    #Generar el token
+    access_token=create_access_token(identity=user.id)
+    #Retornar el token
+    return jsonify({"accessToken":access_token})
+
+@api.route("/helloprotected", methods=["GET"])
+@jwt_required()
+def hello_protected_get():
+    user_id=get_jwt_identity()
+    return jsonify({"userId":user_id, "message":"Hello protected route"})
